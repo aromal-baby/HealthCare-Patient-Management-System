58ff77948129735aa51696d72a6110b5199320aa
aromal-baby <aromalbaby365@gmail.com>
2025-05-12 02:05:22 +0200

    Starting the Health Care patient Management System as the part og Advanced Databases Reassessment

---
+++ src/main/java/com/example/healthcarepatientmanagement/HealthCarePatientManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthcarepatientmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCarePatientManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthCarePatientManagementApplication.class, args);
+    }
+
+}
